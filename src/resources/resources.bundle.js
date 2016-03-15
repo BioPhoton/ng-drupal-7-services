c219c8adf30adfe240cb2d30b0f95011bc0c84cc
BioPhoton <michael@hladky.at>
2016-03-16 00:04:52 +0100

    fixed missing file

--- src/resources/resources.bundle.js
+++ src/resources/resources.bundle.js
@@ -6,17 +6,17 @@
    * @name d7-services.resources:Resources
    * @description
    * This Module bundles all modules related to drupals resources
-   * @requires d7-services.resources.comment.bundle:CommentBundle
-   * @requires d7-services.resources.definition.bundle:DefinitionBundle
-   * @requires d7-services.resources.file.bundle:FileBundle
-   * @requires d7-services.resources.geocoder.bundle:GoecoderBundle
-   * @requires d7-services.resources.menu.bundle:MenuBundle
-   * @requires d7-services.resources.node.bundle:NodeBundle
+   * @requires d7-services.resources.comment:CommentBundle
+   * @requires d7-services.resources.definition:DefinitionBundle
+   * @requires d7-services.resources.file:FileBundle
+   * @requires d7-services.resources.geocoder:GoecoderBundle
+   * @requires d7-services.resources.menu:MenuBundle
+   * @requires d7-services.resources.node:NodeBundle
    * @requires d7-services.resources.system:System
-   * @requires d7-services.resources.taxonomy_term.bundle:TaxonomyTermBundle
-   * @requires d7-services.resources.taxonomy_vocabulary.bundle:TaxonomyVocabularyBundle
-   * @requires d7-services.resources.user.bundle:UserBundle
-   * @requires d7-services.resources.views.bundle:ViewsBundle
+   * @requires d7-services.resources.taxonomy_term:TaxonomyTermBundle
+   * @requires d7-services.resources.taxonomy_vocabulary:TaxonomyVocabularyBundle
+   * @requires d7-services.resources.user:UserBundle
+   * @requires d7-services.resources.views:ViewsBundle
    *
    */
   angular
